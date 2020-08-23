e300daf5002862b0da655f8148dfe86d83ac2c3b
User <User@MSI>
2020-08-23 23:55:55 +0700

    Add project files.

---
+++ Length Wow/LengthWowSource.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#define _CRT_SECURE_NO_WARNINGS
+int main()
+{
+	int level;
+	printf("Enter fav number : ");
+	scanf_s("%d", &level);
+	for (int row = 0; row < level; row++) {
+		for (int star = level - row; star > 0; star--) {
+			printf("Wow");
+		}
+		printf("\n");
+	}
+	return 0;
+}
